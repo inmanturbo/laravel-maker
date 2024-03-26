90337424ffdace3af42c98944b2803021a74cea1
= <inman.turbo@gmail.com>
2024-03-25 20:31:37 -0400

    work in progress

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,33 +1,26 @@
 #!/bin/bash
 
-# Temporary directory for cloning
 temp_dir="temp_laravel_repo"
 
-# Repository details
 repo_url="https://github.com/laravel/laravel.git"
 branch="11.x"
 bootstrap_directory="bootstrap"
 
-# Clone the specific branch and directory
 git clone --depth 1 --filter=blob:none --sparse $repo_url $temp_dir
 cd $temp_dir
 git sparse-checkout set $bootstrap_directory
 
-# Move the directory out
 mv $bootstrap_directory ../
 cd ..
 
-# Check if bootstrap/cache exists, if not create it and add .gitignore
 if [ ! -d "bootstrap/cache" ]; then
     mkdir -p "bootstrap/cache"
     echo -e "*\n!.gitignore" > "bootstrap/cache/.gitignore"
 fi
 
-# Create storage/framework/cache directory and add .gitignore
 mkdir -p "storage/framework/views"
 echo -e "*\n!.gitignore" > "storage/framework/views/.gitignore"
 
-# Cleanup
 rm -rf $temp_dir
 
 echo "The '$bootstrap_directory' directory has been downloaded and configured."
--- install.sh
+++ install.sh
@@ -4,3 +4,7 @@ curl -o maker "https://raw.githubusercontent.com/inmanturbo/laravel-maker/main/m
 chmod +x maker
 curl "https://raw.githubusercontent.com/inmanturbo/laravel-maker/main/bootstrap.sh" | bash
 composer require --dev laravel/framework laravel/pint laravel/tinker spatie/laravel-ignition
+
+if [ -d "src" ]; then
+    ln -s "$(pwd)/src" "$(pwd)/app"
+fi
